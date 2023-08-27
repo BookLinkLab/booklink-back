05e81eacf08429f7e040e060633eacecf13b8219
JuanSeZ <juan.zanelli@ing.austral.edu.ar>
2023-08-26 23:20:31 -0300

    feat: add cors config

--- src/main/java/com/booklink/backend/config/SecurityConfig.java
+++ src/main/java/com/booklink/backend/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,7 +25,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authz -> authz
+        http
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(authz -> authz
                         .anyRequest()
                         .permitAll())
                 .csrf(AbstractHttpConfigurer::disable);
