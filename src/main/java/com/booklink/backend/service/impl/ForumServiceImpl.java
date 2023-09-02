78f3cb775b8c53e5a9c4d735f63193045f329992
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-09-02 20:30:56 -0300

    feat: add Tag to Forum

--- src/main/java/com/booklink/backend/service/impl/ForumServiceImpl.java
+++ src/main/java/com/booklink/backend/service/impl/ForumServiceImpl.java
@@ -2,10 +2,16 @@ package com.booklink.backend.service.impl;
 
 import com.booklink.backend.dto.forum.CreateForumDto;
 import com.booklink.backend.dto.forum.ForumDto;
+import com.booklink.backend.dto.tag.CreateTagDto;
 import com.booklink.backend.dto.user.UserDto;
 import com.booklink.backend.dto.user.UserProfileDto;
+import com.booklink.backend.exception.AlreadyAssignedException;
+import com.booklink.backend.exception.NotFoundException;
+import com.booklink.backend.exception.UserNotAdminException;
 import com.booklink.backend.model.Forum;
+import com.booklink.backend.model.Tag;
 import com.booklink.backend.repository.ForumRepository;
+import com.booklink.backend.service.TagService;
 import com.booklink.backend.service.UserService;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +21,12 @@ import java.util.List;
 public class ForumServiceImpl implements com.booklink.backend.service.ForumService {
     private final ForumRepository forumRepository;
     private final UserService userService;
+    private final TagService tagService;
 
-    public ForumServiceImpl(ForumRepository forumRepository, UserService userService) {
+    public ForumServiceImpl(ForumRepository forumRepository, UserService userService, TagService tagService) {
         this.forumRepository = forumRepository;
         this.userService = userService;
+        this.tagService = tagService;
     }
 
     @Override
@@ -33,5 +41,15 @@ public class ForumServiceImpl implements com.booklink.backend.service.ForumServi
     public List<ForumDto> getAllForums() {
         List<Forum> forums = forumRepository.findAll();
         return forums.stream().map(ForumDto::from).toList();
+    }
+
+    @Override
+    public ForumDto addTagToForum(Long forumId, Long userId, CreateTagDto createTagDto) {
+        Forum forum = forumRepository.findById(forumId).orElseThrow(() -> new NotFoundException("Foro %d no encontrado".formatted(forumId)));
+        if (!forum.getUserId().equals(userId)) throw new UserNotAdminException("Usuario %d no es el administrador del foro %d".formatted(userId, forumId));
+        Tag tag = tagService.findOrCreateTag(createTagDto);
+        if (forum.getTags().contains(tag)) throw new AlreadyAssignedException("Etiqueta %s ya asignada al foro %d".formatted(tag.getName(), forumId));
+        Forum savedForum = forumRepository.save(forum);
+        return ForumDto.from(savedForum);
     }
 }
