345b3fa2ae01b42a26395deed154b2b628585c92
nicolas-mangini <nicolas.mangini@ing.austral.edu.ar>
2023-09-08 19:29:11 -0300

    refactor: change exceptions language

--- src/main/java/com/booklink/backend/service/impl/ForumServiceImpl.java
+++ src/main/java/com/booklink/backend/service/impl/ForumServiceImpl.java
@@ -48,7 +48,7 @@ public class ForumServiceImpl implements com.booklink.backend.service.ForumServi
 
     @Override
     public ForumDto addTagToForum(Long forumId, Long userId, CreateTagDto createTagDto) {
-        Forum forum = forumRepository.findById(forumId).orElseThrow(() -> new NotFoundException("El foro no fue encontrado"));
+        Forum forum = forumRepository.findById(forumId).orElseThrow(() -> new NotFoundException("El foro %s no fue encontrado".formatted(forumId)));
         if (!forum.getUserId().equals(userId)) throw new UserNotAdminException("No eres es el administrador del foro");
         Tag tag = tagService.findOrCreateTag(createTagDto);
         if (forumRepository.existsByIdAndTagsContaining(forumId, tag)) throw new AlreadyAssignedException("La etiqueta %s ya fue asignada al foro".formatted(tag.getName()));
@@ -60,7 +60,7 @@ public class ForumServiceImpl implements com.booklink.backend.service.ForumServi
     @Override
     public ForumDto editForum(Long forumId, Long userId, EditForumDto editForumDto) {
         Optional<Forum> forumOptional = forumRepository.findById(forumId);
-        Forum forumToEdit = forumOptional.orElseThrow(() -> new NotFoundException("El foro no fue encontrado"));
+        Forum forumToEdit = forumOptional.orElseThrow(() -> new NotFoundException("El foro %s no fue encontrado".formatted(forumId)));
         if (!forumToEdit.getUserId().equals(userId)) throw new UserNotAdminException("No eres es el administrador del foro %s".formatted(forumToEdit.getName()));
         forumToEdit.setName(editForumDto.getName());
         forumToEdit.setDescription(editForumDto.getDescription());
@@ -92,7 +92,7 @@ public class ForumServiceImpl implements com.booklink.backend.service.ForumServi
     @Override
     public Forum getForumEntityById(Long id) {
         Optional<Forum> forumOptional = forumRepository.findById(id);
-        return forumOptional.orElseThrow(() -> new NotFoundException("El foro no fue encontrado".formatted(id)));
+        return forumOptional.orElseThrow(() -> new NotFoundException("El foro %s no fue encontrado".formatted(id)));
     }
 
     @Override
--- src/main/java/com/booklink/backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/booklink/backend/service/impl/UserServiceImpl.java
@@ -54,13 +54,13 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUserEntityById(Long id) {
         Optional<User> userOptional = this.userRepository.findById(id);
-        return userOptional.orElseThrow(() -> new NotFoundException("User %s not found".formatted(id)));
+        return userOptional.orElseThrow(() -> new NotFoundException("El usuario %s no fue encontrado".formatted(id)));
     }
 
     @Override
     public UserDto updateUser(long id, UpdateUserDto updateUserDTO) {
         Optional<User> userOptional = this.userRepository.findById(id);
-        User user = userOptional.orElseThrow(() -> new NotFoundException("El Usuario no fue encontrado"));
+        User user = userOptional.orElseThrow(() -> new NotFoundException("El Usuario %s no fue encontrado".formatted(id)));
         user.setEmail(updateUserDTO.getEmail());
         user.setUsername(updateUserDTO.getUsername());
         String encryptedPassword = this.passwordEncoder.encode(updateUserDTO.getPassword());
@@ -72,7 +72,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto authorizedGetByEmail(LoginRequestDto loginRequestDto) {
         Optional<User> userOptional = this.userRepository.findByEmail(loginRequestDto.getEmail());
-        User user = userOptional.orElseThrow(() -> new NotFoundException("El usuario no fue encontrado"));
+        User user = userOptional.orElseThrow(() -> new NotFoundException("El usuario %s no fue encontrado".formatted(loginRequestDto.getEmail())));
         boolean passwordMatches = this.passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword());
         if (!passwordMatches) {
             throw new WrongCredentialsException("Credenciales invalidas");
