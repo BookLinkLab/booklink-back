52bb76ffa5a3f9325eaee2b07f311f454454303f
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-09-15 11:27:51 -0300

    feat: edit forum with optional values

--- src/main/java/com/booklink/backend/dto/forum/EditForumDto.java
+++ src/main/java/com/booklink/backend/dto/forum/EditForumDto.java
@@ -1,6 +1,8 @@
 package com.booklink.backend.dto.forum;
 
+import com.booklink.backend.annotations.NullOrNotBlank;
 import com.booklink.backend.dto.tag.CreateTagDto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -17,12 +19,12 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class EditForumDto {
-    @NotBlank(message = "El nombre del foro no puede estar vacío")
+    @NullOrNotBlank(message = "El nombre del foro no puede estar vacío")
     @Size(min = 4, max = 32, message = "El nombre del foro debe tener entre 4 y 32 caracteres")
     private String name;
 
-    @NotBlank(message = "La descripción no debe estar vacía")
-    @Size(max = 512)
+    @NullOrNotBlank(message = "La descripción no puede estar vacía")
+    @Size(max = 512, message = "La descripción no puede tener más de 512 caracteres")
     private String description;
 
     private List<CreateTagDto> tags;
