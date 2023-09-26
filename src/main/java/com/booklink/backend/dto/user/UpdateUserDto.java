a47d6a6518933f408a35df30ba4c075c38fb2de9
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-09-26 18:52:14 -0300

    fix: password pattern validation

--- src/main/java/com/booklink/backend/dto/user/UpdateUserDto.java
+++ src/main/java/com/booklink/backend/dto/user/UpdateUserDto.java
@@ -20,5 +20,7 @@ public class UpdateUserDto {
     @Email
     private String email;
 
+    @Size(min = 8, max = 24, message = "La contraseña debe tener entre 8 y 24 caracteres")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "La contraseña solo debe contener letras y números")
     private String password;
 }
