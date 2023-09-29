39e362d4f500898b1951c5badaebc6ce78883ff1
nicolas <nicolas.mangini@ing.austral.edu.ar>
2023-09-29 15:22:42 -0300

    feat: delegate private methods from CommentControllerTest  to ControllerTestUtils

--- src/test/java/com/booklink/backend/controller/CommentControllerTest.java
+++ src/test/java/com/booklink/backend/controller/CommentControllerTest.java
@@ -1,27 +1,21 @@
 package com.booklink.backend.controller;
 
-import com.booklink.backend.dto.LoginRequestDto;
-import com.booklink.backend.dto.LoginResponseDto;
 import com.booklink.backend.dto.comment.CommentDto;
 import com.booklink.backend.dto.comment.CreateCommentDto;
 import com.booklink.backend.dto.forum.ForumDto;
-import com.booklink.backend.dto.post.CreatePostDto;
 import com.booklink.backend.dto.post.PostDto;
-import com.booklink.backend.dto.user.CreateUserDto;
 import com.booklink.backend.dto.user.UserDto;
+import com.booklink.backend.utils.ControllerTestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 
-import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -32,19 +26,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CommentControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
+    @Autowired
+    private ControllerTestUtils utils;
 
     private final String baseUrl = "/comment";
 
     @BeforeEach
     void setup() {
-        UserDto user = createUserAndLogIn("user", "user@email.com", "password");
-        Long forum = 1L;
-        ForumDto forumJoined = joinUserToForum(forum);
-        PostDto post = createPost(forum, "This is a test post");
+        UserDto user = utils.createUserAndLogIn("user", "user@email.com", "password");
+        ForumDto forumJoined = utils.joinUserToForum(1L);
+        PostDto post = utils.createPost(1L, "This is a test post");
     }
 
     @Test
-    void createCommentTest(){
+    void createCommentTest() {
         CreateCommentDto createCommentDto = CreateCommentDto.builder()
                 .postId(1L)
                 .content("This is a test comment")
@@ -57,7 +52,7 @@ public class CommentControllerTest {
 
     @Test
     void createCommentWhenNotMemberTest() {
-        createUserAndLogIn("notForumMember", "notForumMember@email.com", "password");
+        utils.createUserAndLogIn("notForumMember", "notForumMember@email.com", "password");
         CreateCommentDto createCommentDto = CreateCommentDto.builder()
                 .postId(1L)
                 .content("The creator of this comment is not member of the forum")
@@ -65,45 +60,5 @@ public class CommentControllerTest {
 
         ResponseEntity<String> response = restTemplate.postForEntity(baseUrl, createCommentDto, String.class);
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-    }
-
-    private UserDto createUserAndLogIn(String username, String email, String password) {
-        CreateUserDto createUserDto = CreateUserDto.builder()
-                .username(username)
-                .email(email)
-                .password(password)
-                .build();
-        ResponseEntity<LoginResponseDto> createUserResponse = restTemplate.postForEntity("/user", createUserDto, LoginResponseDto.class);
-
-        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
-                .email(email)
-                .password(password)
-                .build();
-        ResponseEntity<LoginResponseDto> response = restTemplate.postForEntity(
-                "/auth", loginRequestDto, LoginResponseDto.class
-        );
-        String token = Objects.requireNonNull(response.getBody()).getToken();
-        restTemplate.getRestTemplate().setInterceptors(
-                List.of((request, body, execution) -> {
-                    request.getHeaders().add("Authorization", "Bearer " + token);
-                    return execution.execute(request, body);
-                })
-        );
-        return Objects.requireNonNull(createUserResponse.getBody()).getUser();
-    }
-
-    private PostDto createPost(Long forumId, String content) {
-        CreatePostDto createPostDto = CreatePostDto.builder()
-                .forumId(forumId)
-                .content(content)
-                .build();
-
-        ResponseEntity<PostDto> response = restTemplate.postForEntity("/post", createPostDto, PostDto.class);
-        return response.getBody();
-    }
-
-    private ForumDto joinUserToForum(Long forumId) {
-        ResponseEntity<ForumDto> response = restTemplate.exchange("/forum/%s/join".formatted(forumId), HttpMethod.POST, new HttpEntity<>(null), ForumDto.class);
-        return response.getBody();
     }
 }
--- src/test/java/com/booklink/backend/utils/ControllerTestUtils.java
+++ src/test/java/com/booklink/backend/utils/ControllerTestUtils.java
@@ -1,12 +1,70 @@
 package com.booklink.backend.utils;
 
+import com.booklink.backend.dto.LoginRequestDto;
+import com.booklink.backend.dto.LoginResponseDto;
+import com.booklink.backend.dto.forum.ForumDto;
+import com.booklink.backend.dto.post.CreatePostDto;
+import com.booklink.backend.dto.post.PostDto;
+import com.booklink.backend.dto.user.CreateUserDto;
+import com.booklink.backend.dto.user.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
+
+@Service
 public class ControllerTestUtils {
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     public static void setOutputStreamingFalse(TestRestTemplate restTemplate) {
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
         restTemplate.getRestTemplate().setRequestFactory(requestFactory);
+    }
+
+    public UserDto createUserAndLogIn(String username, String email, String password) {
+        CreateUserDto createUserDto = CreateUserDto.builder()
+                .username(username)
+                .email(email)
+                .password(password)
+                .build();
+        ResponseEntity<LoginResponseDto> createUserResponse = restTemplate.postForEntity("/user", createUserDto, LoginResponseDto.class);
+
+        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
+                .email(email)
+                .password(password)
+                .build();
+        ResponseEntity<LoginResponseDto> response = restTemplate.postForEntity(
+                "/auth", loginRequestDto, LoginResponseDto.class
+        );
+        String token = Objects.requireNonNull(response.getBody()).getToken();
+        restTemplate.getRestTemplate().setInterceptors(
+                List.of((request, body, execution) -> {
+                    request.getHeaders().add("Authorization", "Bearer " + token);
+                    return execution.execute(request, body);
+                })
+        );
+        return Objects.requireNonNull(createUserResponse.getBody()).getUser();
+    }
+
+    public PostDto createPost(Long forumId, String content) {
+        CreatePostDto createPostDto = CreatePostDto.builder()
+                .forumId(forumId)
+                .content(content)
+                .build();
+
+        ResponseEntity<PostDto> response = restTemplate.postForEntity("/post", createPostDto, PostDto.class);
+        return response.getBody();
+    }
+
+    public ForumDto joinUserToForum(Long forumId) {
+        ResponseEntity<ForumDto> response = restTemplate.exchange("/forum/%s/join".formatted(forumId), HttpMethod.POST, new HttpEntity<>(null), ForumDto.class);
+        return response.getBody();
     }
 }
