61ca4892d0560e9adb1d6241e6e536e77cada9eb
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-09-30 15:56:23 -0300

    test: get post tests

--- src/test/java/com/booklink/backend/service/PostServiceTest.java
+++ src/test/java/com/booklink/backend/service/PostServiceTest.java
@@ -3,6 +3,7 @@ package com.booklink.backend.service;
 
 import com.booklink.backend.dto.post.CreatePostDto;
 import com.booklink.backend.dto.post.PostDto;
+import com.booklink.backend.dto.post.PostViewDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -50,6 +52,21 @@ public class PostServiceTest {
 
         postService.createPost(createPostDto2, 1L);
         assertEquals(2, postService.getPostsByForumId(1L).size());
+    }
+
+    @Test
+    public void getPostById() {
+        CreatePostDto createPostDto = CreatePostDto.builder()
+                .forumId(1L)
+                .content("This is a test post")
+                .build();
+        forumService.joinForum(1L, 1L);
+
+        PostDto postDto = postService.createPost(createPostDto, 1L);
+
+        PostViewDto postViewDto = postService.getPostViewById(postDto.getId());
+        assertEquals("This is a test post", postViewDto.getContent());
+        assertTrue(postViewDto.getComments().isEmpty());
     }
 
 
