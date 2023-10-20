868044812d68e39016327e69935979c02af5b24b
JuanSeZ <juan.zanelli@ing.austral.edu.ar>
2023-10-20 09:45:18 -0300

    feat: add likes and dislikes in dtos

--- src/main/java/com/booklink/backend/dto/post/PostInfoDto.java
+++ src/main/java/com/booklink/backend/dto/post/PostInfoDto.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -17,6 +18,8 @@ public class PostInfoDto {
     private UserPostDto user;
     private String content;
     private Date date;
+    private List<Long> likes;
+    private List<Long> dislikes;
 
     public static PostInfoDto from(Post post) {
         return PostInfoDto.builder()
@@ -24,6 +27,8 @@ public class PostInfoDto {
                 .user(UserPostDto.from(post.getUser()))
                 .content(post.getContent())
                 .date(post.getCreatedDate())
+                .likes(post.getLikes())
+                .dislikes(post.getDislikes())
                 .build();
     }
 }
